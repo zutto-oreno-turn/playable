054bb3c107eb992317a7fd3054f2628b3d5f65ab
kotaro36 <yamamoto.kintaro@gmail.com>
2021-01-30 21:44:56 +0900

    Modify 384px

---
+++ Assets/Playable/Scripts/GroundComponent.cs
@@ -0,0 +1,7 @@
+﻿using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct GroundComponent : IComponentData
+{
+    public float Speed;
+}
---
+++ Assets/Playable/Scripts/GroundSystem.cs
@@ -0,0 +1,40 @@
+﻿using Unity.Entities;
+using Unity.Tiny;
+
+public class GroundSystem : SystemBase
+{
+    enum ProgressState
+    {
+        Initializing,
+        Playing
+    }
+
+    ProgressState State = ProgressState.Initializing;
+
+    protected override void OnUpdate()
+    {
+        switch (State)
+        {
+            case ProgressState.Initializing:
+                Initialization();
+                break;
+            case ProgressState.Playing:
+                Play();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Initialization()
+    {
+        State = ProgressState.Playing;
+        DisplayInfo di = GetSingleton<DisplayInfo>();
+        UnityEngine.Debug.Log($"GroundSystem#Initialization1 di.width: {di.width}, di.height: {di.height}");
+    }
+
+    void Play()
+    {
+        UnityEngine.Debug.Log("GroundSystem#Play");
+    }
+}
